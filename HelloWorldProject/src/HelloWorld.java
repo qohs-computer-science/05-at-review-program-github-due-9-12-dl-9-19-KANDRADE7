7cf913ee54217b782fb77daac469ee2fb7e87591
KANDRADE7 <andradekate@icloud.com>
2025-09-10 16:45:07 +0000

    end of class 9/10

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,14 +1,52 @@
-/*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
- */
+//Katherine Andrade, 9/10/25, period 7, basic user input and output program
 //Yes, I know the file name should be something like ReviewProgram - working on it!
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		//TODO: Implement Program Requirements Here
-	}
 
-}
+		Scanner input = new Scanner(System.in);
+		System.out.print("\nEnter your name: ");
+		String userName = input.nextLine();
+
+		System.out.print("Enter your age: ");
+		int age = input.nextInt();
+		input.nextLine();
+
+		System.out.print("Enter your favorite 3 digit number: ");
+		int favNum = input.nextInt();
+
+		System.out.print("Enter your GPA: ");
+		double userGPA = input.nextDouble();
+
+
+		//output
+		System.out.println("\nWelcome " + userName + "!");
+
+		if(age > 18)
+			System.out.println("Congratulation you can vote!");
+		if(age < 18)
+			System.out.println("You have " + (18-age) + " years till you can vote!");
+
+		if(userGPA >= 4.5)
+			System.out.println("Your hard work is paying off! Keep up the great work!");
+		else if(userGPA >= 4 && userGPA <=4.49)
+			System.out.println("Keep up the great work!");
+		else if( userGPA >=3.5 && userGPA <=3.99)
+			System.out.println("Continue trying!");
+		else if(userGPA >=3 && userGPA <=3.49)
+			System.out.println("Keep studying!");
+		else
+			System.out.println("Ask for help from your teachers and change your study methods. You can do anything you set your mind to.");
+
+		int userNameLen =  userName.length();
+		System.out.println(userNameLen);
+
+		for(int x =userNameLen; x<=favNum; x+=userNameLen){
+			System.out.print(x + ",");
+		}//end loop
+
+	}//end main
+
+}//end class
